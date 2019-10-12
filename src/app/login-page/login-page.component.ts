86a265053f51e91e42369d0d636826947edd5170
DjDeath87 <djdeath1987@gmail.com>
2019-10-12 03:07:06 +0300

    My Project commit

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,7 +1,7 @@
 import {Component, Injectable, OnInit} from '@angular/core';
 import {AuthService} from '../services/auth.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-// import * as bcrypt from 'bcryptjs';
+// import *  as bcrypt from 'bcryptjs';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
